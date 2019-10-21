1d0495a7716f6611d1bfaa7aa6898fc77d0a3fb1
Khalilovic3 <khalilovic3@etf.unsa.ba>
2019-10-21 18:40:43 +0200

    Prvi

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -6,35 +6,69 @@ public class Interval {
     private boolean PripadaPoc;
     private boolean PripadaKraj;
 
+    public double getPocetna() {
+        return pocetna;
+    }
+
+    public void setPocetna(double pocetna) {
+        this.pocetna = pocetna;
+    }
+
+    public double getKraj() {
+        return kraj;
+    }
+
+    public void setKraj(double kraj) {
+        this.kraj = kraj;
+    }
+
+    public boolean isPripadaPoc() {
+        return PripadaPoc;
+    }
+
+    public void setPripadaPoc(boolean pripadaPoc) {
+        PripadaPoc = pripadaPoc;
+    }
+
+    public boolean isPripadaKraj() {
+        return PripadaKraj;
+    }
+
+    public void setPripadaKraj(boolean pripadaKraj) {
+        PripadaKraj = pripadaKraj;
+    }
+
+
+
     public Interval(double p,double k,boolean PP,boolean PK){
         try {
             if (p > k) {
                 throw new IllegalArgumentException("Neispravno");
             }
-            pocetna = p;
-            kraj = k;
-            PripadaPoc = PP;
-            PripadaKraj = PK;
+            this.pocetna = p;
+            this.kraj = k;
+            this.PripadaPoc = PP;
+            this.PripadaKraj = PK;
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
     }
     public Interval(){
-        pocetna = 0;
-        kraj = 0;
-        PripadaKraj = false;
-        PripadaPoc = false;
+        this.pocetna = 0;
+        this.kraj = 0;
+        this.PripadaKraj = false;
+        this.PripadaPoc = false;
     }
 
     final boolean isNull(){
-        if(pocetna == 0 && kraj == 0 && !PripadaPoc && !PripadaKraj){
+        if(this.getPocetna() == 0 && this.getKraj() == 0 && !this.isPripadaKraj() && !this.isPripadaPoc()){
             return true;
         }else{
             return false;
         }
     }
     final boolean isIn(double a){
-        if(a >= pocetna && a <= kraj){
+        if(a >= this.getPocetna() && a <= this.getKraj()){
             return true;
         }else{
             return false;
@@ -44,8 +78,23 @@ public class Interval {
 
     }
     static Interval intersect (Interval a,Interval b){
-
-    }
+        Interval temp = new Interval();
+        if(a.getPocetna() > b.getPocetna()){
+            temp.setPocetna(a.getPocetna());
+            temp.setPripadaPoc(a.isPripadaPoc());
+        }else if(a.getPocetna() < b.getPocetna()){
+            temp.setPocetna(b.getPocetna());
+            temp.setPripadaPoc(b.isPripadaPoc());
+        }else{
+            temp.setPocetna(b.getPocetna());
+            if(a.isPripadaPoc() == false){
+                temp.setPripadaPoc(false);
+            }else if(b.isPripadaPoc() == false){
+                temp.setPripadaPoc(false);
+            }else{
+                temp.setPripadaPoc(true);
+            }
+        }
     @Override
     public String toString(){
 
